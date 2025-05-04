adfb965a1ebeca794f8e1e5f05b682c9798c9615
Don Dikaio <d@dika.io>
2025-05-04 09:35:04 -0700

    chore: bump version to v0.4.2 and add empty go.sum

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -18,7 +18,7 @@ import (
 // Version information set by build flags
 var (
 	// Version is the semantic version of the application
-	Version = "v0.3.2"
+	Version = "v0.4.2"
 	// Commit is the git commit SHA at build time
 	Commit = "none" 
 	// Date is the build date
